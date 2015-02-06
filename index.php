8e8606102f8d502cb5460697c176aa6d5bf58d10
Roque Arnas <roque.arnas@didesweb.com>
2015-02-06 01:13:47 +0100

    Create index.php

---
+++ index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE HTML>
+<html lang="es">
+<head>
+	<meta charset="UTF-8">
+	<title>Didesweb</title>
+</head>
+<body>
+	<h1>Expresiones regulares en php</h1>
+	<br>
+	<form action="pruebasformulariosexpresionesregulares.php" method="post">
+	Nombre: <input type="text" name="nombre" id="nombre">
+	<br><br>
+	Correo: <input type="text" name="correo" id="correo">
+	<br><br>
+	Contraseña: <input type="text" name="contraseña" id="contraseña">
+	<br><br>
+	Edad: <input type="text" name="edad" id="edad">
+	<br><br>
+	Telefono: <input type="text" name="telefono" id="telefono">
+	<br><br>
+	Movil: (+34) <input type="text" name="movil" id="movil">
+	<br><br>
+	<input type="submit" value="Enviar">	
+	</form>
+</body>
+</html>
